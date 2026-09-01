cf626d5860317d61aeeb9f4b65798d4e12d8b8bf
Gabriel <gabriel.p.alves12@aluno.senai.br>
2026-09-01 10:04:50 -0300

    envio 2

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POO</title>
+</head>
+<body>
+    <h2>Programação Orientada a Objeto</h2>
+</body>
+</html>
